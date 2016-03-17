193392a59b4ffae524c4d31464d382f0758be1b6
phillipcheng <phillipchengyi@gmail.com>
2016-03-17 07:50:31 -0700

    added

---
+++ leetcode/leetcode/src/main/java/princeton/algo1/interview/QueueWith2Stacks.java
@@ -0,0 +1,6 @@
+package princeton.algo1.interview;
+
+public class QueueWith2Stacks {
+// Implement a queue with two stacks so that each queue operations takes a constant amortized number of stack operations.
+	
+}
---
+++ leetcode/leetcode/src/main/java/princeton/algo1/interview/TossEggs.java
@@ -0,0 +1,16 @@
+package princeton.algo1.interview;
+
+public class TossEggs {
+	
+	/*
+Egg drop. Suppose that you have an N-story building (with floors 1 through N) and plenty of eggs. 
+An egg breaks if it is dropped from floor T or higher and does not break otherwise. 
+Your goal is to devise a strategy to determine the value of T given the following limitations on the number of eggs and tosses:
+Version 0: 1 egg, ¡ÜT tosses.
+Version 1: lgN eggs and lgN tosses.
+Version 2: lgT eggs and 2lgT tosses.
+Version 3: 2 eggs and 2*sqr(N) tosses.
+Version 4: 2 eggs and 2*sqrt(2)*sqrt(T) tosses
+	 */
+
+}
