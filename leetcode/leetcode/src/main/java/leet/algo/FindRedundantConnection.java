ca271a10f3056786a2caf8e49368047b283477f4
iris.zj <iris.zj@antfin.com>
2021-01-14 08:27:04 +0800

    add

--- leetcode/leetcode/src/main/java/leet/algo/FindRedundantConnection.java
+++ leetcode/leetcode/src/main/java/leet/algo/FindRedundantConnection.java
@@ -58,51 +58,40 @@ public class FindRedundantConnection {
             return edgeToIdx.get(toStr(edge));
         }
 
-        //return the circle path
-        public List<List<Integer>> getCircle(int start){
-            Stack<Integer> stack = new Stack<>();//stack of nodes to visit
-            stack.push(start);
-            Map<Integer, List<Integer>> pushPath = new HashMap<>();
-            boolean finish=false;
-            int toN=-1;
-            int fromNode = -1;
-            int[] parent = new int[nodes.size()+1];
-            for (int i=1; i<parent.length; i++){
-                parent[i] = -1;
-            }
-            while(!stack.isEmpty() || !finish){
-                fromNode = stack.pop();//visit now
-                Set<Integer> toNodes = edges.get(fromNode);
-                if (toNodes!=null) {
-                    for (int toNode : toNodes) {
-                        int father = parent[fromNode];
-                        if (father != toNode) {
-                            logger.info("stack:" + stack);
-                            logger.info("pushPath:" + pushPath);
-                            if (pushPath.containsKey(toNode)) {
-                                toN = toNode;
-                                finish = true;
-                                break;
-                            } else {//
-                                stack.push(toNode);
-                                pushPath.put(toNode, Arrays.asList(new Integer[]{fromNode, toNode}));
-                                parent[toNode] = fromNode;
-                                logger.info(String.format("add path: %s -> %s", fromNode, toNode));
+        /**
+         *
+         * @param node, start
+         * @param ancestor
+         * @return null if no circle, return all edges belongs to the circle
+         */
+        List<List<Integer>> dfs(int node, Stack<Integer> ancestor){
+            logger.info("node:"+node + ", stack:"+ ancestor);
+            Set<Integer> toNodes = edges.get(node);
+            if (toNodes != null) {
+                for (Integer toNode : toNodes) {
+                    if (ancestor.size()==0 || ancestor.peek() != toNode) {
+                        List<List<Integer>> ret;
+                        List<Integer> edge = new ArrayList<>();
+                        edge.add(node);
+                        edge.add(toNode);
+                        if (ancestor.contains(toNode)) {//circle detected
+                            ret = new ArrayList<>();
+                            ret.add(edge);
+                            return ret;
+                        } else {
+                            Stack<Integer> newAncestor = new Stack<>();
+                            newAncestor.addAll(ancestor);
+                            newAncestor.add(node);
+                            ret = dfs(toNode, newAncestor);
+                            if (ret != null) {//has circle
+                                ret.add(edge);
+                                return ret;
                             }
                         }
                     }
                 }
             }
-            //create circle from prev->toNode is the last edge
-            List<List<Integer>> path = new ArrayList<>();
-            path.add(Arrays.asList(new Integer[]{fromNode, toN}));
-            int node = fromNode;
-            while (node != -1){
-                int father = parent[node];
-                path.add(Arrays.asList(new Integer[]{father, node}));
-                node = father;
-            }
-            return path;
+            return null;
         }
     }
 
@@ -114,28 +103,52 @@ public class FindRedundantConnection {
             graph.addEdge(edge[1], edge[0], i);
         }
         int start = edges[0][0];
-        List<List<Integer>> path = graph.getCircle(start);
+        List<List<Integer>> path = graph.dfs(start, new Stack<>());
+        logger.info("path:" + path);
         TreeMap<Integer, int[]> idxToEdge = new TreeMap<>();
         for (List<Integer> edge: path){
             int idx = graph.getIdx(edge);
             idxToEdge.put(idx, new int[]{edge.get(0), edge.get(1)});
         }
-        return idxToEdge.lastEntry().getValue();
+        int[] biggest = idxToEdge.lastEntry().getValue();
+        int tmp;
+        if (biggest[0]>biggest[1]){
+            tmp = biggest[0];
+            biggest[0] = biggest[1];
+            biggest[1] = tmp;
+        }
+        return biggest;
     }
 
     public static void main(String[] args){
         FindRedundantConnection findRedundantConnection = new FindRedundantConnection();
         int[] ret;
         String strRet;
-//
+
 //        ret = findRedundantConnection.findRedundantConnection(new int[][]{{1,2},{1,3},{2,3}});
 //        strRet = Arrays.toString(ret);
 //        logger.info(strRet);
 //        assertTrue(strRet.equals("[2, 3]"));
+//
+//        ret = findRedundantConnection.findRedundantConnection(new int[][]{{1,2}, {2,3}, {3,4}, {1,4}, {1,5}});
+//        strRet = Arrays.toString(ret);
+//        logger.info(strRet);
+//        assertTrue(strRet.equals("[1, 4]"));
+//
+//        ret = findRedundantConnection.findRedundantConnection(new int[][]{{1,3},{3,4},{1,5},{3,5},{2,3}});
+//        strRet = Arrays.toString(ret);
+//        logger.info(strRet);
+//        assertTrue(strRet.equals("[3, 5]"));
 
-        ret = findRedundantConnection.findRedundantConnection(new int[][]{{1,2}, {2,3}, {3,4}, {1,4}, {1,5}});
+        ret = findRedundantConnection.findRedundantConnection(new int[][]{{30,44},{34,47},{22,32},{35,44},
+                {26,36},{2,15},{38,41},{28,35},{24,37},{14,49},{44,45},{11,50},{20,39},{7,39},{19,22},{3,17},
+                {15,25},{1,39},{26,40},{5,14},{6,23},{5,6},{31,48},{13,22},{41,44},{10,19},{12,41},{1,12},
+                {3,14},{40,50},{19,37},{16,26},{7,25},{22,33},{21,27},{9,50},{24,42},{43,46},{21,47},{29,40},
+                {31,34},{9,31},{14,31},{5,48},{3,18},{4,19},{8,17},{38,46},{35,37},{17,43}});
         strRet = Arrays.toString(ret);
         logger.info(strRet);
-        assertTrue(strRet.equals("[1, 4]"));
+        assertTrue(strRet.equals("[5, 48]"));
+
+
     }
 }
